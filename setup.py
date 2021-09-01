ba1bc39d823e328bcd48a727c401b9ed593d5f95
Pika <lerayplayzz@gmail.com>
2021-09-01 06:09:49 +0200

    Bump to 1.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="hypy-hypixel",
-    version="1.1.1",
+    version="1.1.2",
     description="A Hypixel API Wrapper",
     url="https://github.com/PikaFan123/hypy",
     author="PikaFan123",
